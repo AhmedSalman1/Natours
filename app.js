a23d45a6ce6dde6e47d9a53164266567b1089746
AhmedSalman <medosalman375@gmial.com>
2024-01-22 16:39:44 +0200

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -26,6 +27,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 /*                  1) GLOBAL MIDDLEWARES                  */
+//* Implement CORS
+app.use(cors());
+app.options('*', cors());
+// app.options('/api/v1/tours/:id', cors());
+
 //* Set Security HTTP headers
 // Further HELMET configuration for Security Policy (CSP)
 const scriptSrcUrls = [
