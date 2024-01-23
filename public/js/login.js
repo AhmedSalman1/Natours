44527796ec34c8caae56d96752dcf6611bae4129
AhmedSalman <medosalman375@gmial.com>
2024-01-23 13:07:41 +0200

    Update login

--- public/js/login.js
+++ public/js/login.js
@@ -20,7 +20,7 @@ export const login = async (email, password) => {
             }, 1000);
         }
     } catch (err) {
-        showAlert('error', err.response.data.message);
+        showAlert('error', "The email or password you've entered is wrong.");
     }
 };
 
