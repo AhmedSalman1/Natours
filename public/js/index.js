f5ae4594b04520852bc9e0cb0974ab104a5185a8
AhmedSalman <medosalman375@gmial.com>
2024-01-23 17:32:00 +0200

    Update alert

--- public/js/index.js
+++ public/js/index.js
@@ -104,3 +104,6 @@ if (resetPasswordForm)
         const token = window.location.pathname.split('/').pop();
         resetPassword(password, passwordConfirm, token);
     });
+
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alertMessage) showAlert('success', alertMessage, 20);
