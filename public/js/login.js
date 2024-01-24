931021592872ce18904c8aec1abfd2952023e865
AhmedSalman <medosalman375@gmial.com>
2024-01-24 16:13:07 +0200

    Fix error message

--- public/js/login.js
+++ public/js/login.js
@@ -20,7 +20,7 @@ export const login = async (email, password) => {
             }, 1000);
         }
     } catch (err) {
-        console.error(err.response.data.message);
+        console.error('Error response from server:', err.response);
         showAlert('error', err.response.data.message);
     }
 };
