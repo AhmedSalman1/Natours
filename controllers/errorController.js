c54dae6ad1b493c5eb9399e0953b8965ba21d256
AhmedSalman <medosalman375@gmial.com>
2024-01-25 11:53:14 +0200

    Error Handling Bug Fix

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -95,8 +95,9 @@ module.exports = (err, req, res, next) => {
     if (process.env.NODE_ENV === 'development') {
         sendErrorDev(err, req, res);
     } else if (process.env.NODE_ENV === 'production') {
-        let error = { ...err };
-        error.message = err.message;
+        // let error = { ...err };
+        // error.message = err.message;
+        let error = Object.create(err);
 
         if (error.name === 'CastError') error = handleCastErrorDB(error);
         if (error.code === 11000) error = handleDuplicateFieldsDB(error);
